89f24bb1e7ce774637dedeec81018ab03a141eaa
unknown <psethu27@gmail.com>
2020-06-17 17:13:58 -0400

    create Ride component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RideComponent } from './components/ride/ride.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RideComponent
   ],
   imports: [
     BrowserModule,
