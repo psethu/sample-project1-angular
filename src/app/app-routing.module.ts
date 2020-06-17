db267b4961afff50406fc0a09cde4c0375891d80
unknown <psethu27@gmail.com>
2020-06-17 17:22:50 -0400

    add routing using Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RideComponent } from './components/ride/ride.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RideComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
