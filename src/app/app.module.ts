9ab241b4fe6f5dfa7214fab55d3f965bf7b79b37
unknown <psethu27@gmail.com>
2020-06-18 01:22:49 -0400

    added a validation directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,15 @@ import { AppComponent } from './app.component';
 import { RideComponent } from './components/ride/ride.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { ValidateInputDirective } from './validate-input.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     RideComponent,
     AboutComponent,
-    HeaderComponent
-    
+    HeaderComponent,
+    ValidateInputDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/validate-input.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appValidateInput]'
+})
+export class ValidateInputDirective {
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {  
+    let value:number = parseInt(this.el.nativeElement.value) || 0; // when value is empty string: Nan || 0 evaluates to 0
+    console.log(value);
+      if (value <= 0)
+        this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+      this.el.nativeElement.style.backgroundColor = color;
+  }
+}
